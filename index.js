a120856d66263ffbfa23280ab61fb997ff23e41d
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-11-02 13:38:43 -0300

    Initialize QR code generator with dependencies and basic functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from 'inquirer';
+import qrImage from 'qr-image';
+import fs from 'fs';
+
+
+inquirer.createPromptModule()([
+    {
+        type: 'input',
+        name: 'data',
+        message: 'Enter data to encode as QR code:',
+    },
+    {
+        type: 'input',
+        name: 'filename',
+        message: 'Enter filename for QR code image:',
+    },
+    ]).then((answers) => {
+        const qrCode = qrImage.image(answers.data, { type: 'png' });
+        qrCode.pipe(fs.createWriteStream(`QrCode/${answers.filename}.png`));
+        console.log('QR code generated successfully!');
+    });
